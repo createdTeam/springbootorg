bcabcaedb625cf4b834fc344291af9180b1e9814
yesufang <yesufang@abe9.com>
2020-07-08 13:38:12 +0800

    可能少提交的

--- api/src/main/java/com/springboot/dubbo/api/server/impl/UserServiceImpl.java
+++ api/src/main/java/com/springboot/dubbo/api/server/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.springboot.dubbo.model.mapper.SysUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,5 +25,15 @@ public class UserServiceImpl implements UserService {
     public List<SysUser> list(SysUser sysUser) {
         return mapper.select(sysUser);
 
+    }
+
+    @Override
+    public int addSysUser() {
+        List<SysUser> list = new ArrayList<>();
+        SysUser user1 = new SysUser(1L,"hha");
+        SysUser user2 = new SysUser(2L,"yeye");
+        list.add(user1);
+        list.add(user2);
+        return mapper.insertBatch(list);
     }
 }
