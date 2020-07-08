0c73a962946b1d5b3e032a3832639f00225552dd
yesufang <yesufang@abe9.com>
2020-07-08 13:43:47 +0800

    添加idworker和跨域

--- server/src/main/java/com/springboot/dubbo/server/SpringbootApplication.java
+++ server/src/main/java/com/springboot/dubbo/server/SpringbootApplication.java
@@ -1,8 +1,12 @@
 package com.springboot.dubbo.server;
 
 
+import com.sohu.idcenter.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -12,9 +16,24 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @SpringBootApplication(scanBasePackages = "com.springboot.dubbo")
 @MapperScan("com.springboot.dubbo.model")
-public class SpringbootApplication {
+public class SpringbootApplication extends WebMvcConfigurationSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(0,0,0);
     }
 }
